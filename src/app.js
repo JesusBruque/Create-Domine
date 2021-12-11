476199cbcbd3debff349bbcfcd943594a0350078
JesusBruque <bruke3_voleyutrera@hotmail.com>
2021-12-11 12:05:44 +0000

    feat: Create Domine

--- src/app.js
+++ src/app.js
@@ -9,3 +9,21 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let pronoum = ["the", "our", "my", "you"];
+let adj = ["big", "small", "beautiful", "great"];
+let noum = ["teacher", "artist", "chef"];
+let domine = [".com", ".es", ".org"];
+
+for (let i = 0; i < pronoum.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noum.length; k++) {
+      for (let l = 0; l < domine.length; l++) {
+        console.log(pronoum[i] + adj[j] + noum[k] + domine[l]);
+      }
+    }
+  }
+}
+
+const root = document.querySelector("#root");
+root.innerHTML = "Hola";
